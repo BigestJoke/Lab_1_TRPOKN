d2522e14910b60ff795e20d5c601ff9d820da65a
Nikita Kuznecov <nikita_kuznecov2004@mail.ru>
2024-05-17 06:29:56 +0700

    Добавление комментариев в класс File

--- 1_lab/File.h
+++ 1_lab/File.h
@@ -6,11 +6,13 @@
 
 class File {
 public:
-    File(const QString &filePath);
-    QString getFilePath() const;
-    qint64 getSize() const;
-    QDateTime getLastModified() const;
-    bool exists() const;
+
+    File(const QString &filePath);  // Конструктор
+
+    QString getFilePath() const;// Получение пути к файлу
+    qint64 getSize() const; // Получение размера файла
+    QDateTime getLastModified() const; // Получение времени последнего изменения файла
+    bool exists() const;  // Проверка существования файла
 
 private:
     QFileInfo fileInfo;
